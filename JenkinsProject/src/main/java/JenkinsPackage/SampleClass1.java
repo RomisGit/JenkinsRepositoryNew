f16f15efdf6cbd96e65e2eeb89823003e16f743b
romit <romit@192.168.0.212>
2024-03-13 08:35:11 +0000

    Jenkins class

---
+++ JenkinsProject/src/main/java/JenkinsPackage/SampleClass1.java
@@ -0,0 +1,11 @@
+package JenkinsPackage;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Morning");
+
+	}
+
+}
---
+++ JenkinsProject/src/main/java/JenkinsPackage/SampleClass2.java
@@ -0,0 +1,11 @@
+package JenkinsPackage;
+
+public class SampleClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Romi");
+
+	}
+
+}
