f26891bd920608f8735a606ad534f5350bfe60df
romit <romit@192.168.0.212>
2024-03-13 08:44:36 +0000

    Jenkins New class

--- JenkinsProject/src/main/java/JenkinsPackage/SampleClass1.java
+++ JenkinsProject/src/main/java/JenkinsPackage/SampleClass1.java
@@ -4,7 +4,7 @@ public class SampleClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Good Morning");
+		System.out.println("Good Morning Romi");
 
 	}
 
